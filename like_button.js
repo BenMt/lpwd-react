bb8073acac41e2729b8c6bd0198252e707b06e43
Benoit Maigret <dev@benoitmaigret.com>
2021-03-09 08:18:14 +0100

    Init commit

---
+++ like_button.js
@@ -0,0 +1,39 @@
+'use strict';
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return (
+      <button onClick={() => this.setState({ liked: true })}>
+        J’aime
+      </button>
+    );
+  }
+}
+
+
+
+class MonContainer extends React.Component {
+  render() {
+    return (
+      <div style={{ backgroundColor: 'red' }}>
+        <LikeButton />
+        <LikeButton />
+        <LikeButton />
+        <LikeButton />
+        <LikeButton />
+      </div>
+    )
+  }
+}
+
+const domContainer = document.querySelector('#like_button_container');
+ReactDOM.render(<MonContainer />, domContainer);
